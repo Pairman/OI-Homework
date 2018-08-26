0509ee60e8780f493c5c7364b56f107d2ad7c635
Null <pm2966434705@163.com>
2018-08-26 17:29:49 +0800

    Update and rename 8-16-2018_primes_listing.cpp to 8-26-2018_primes_listing.cpp

--- 8-26-2018_primes_listing.cpp
+++ 8-26-2018_primes_listing.cpp
@@ -1,11 +1,11 @@
-//THIS IS A BUGGY PROGRAM.
 #include <iostream>
 using namespace std;
 
 int isPrime(int n);
 
-int main() {
-	int s, n, times = 0;
+int main() 
+{
+	int s, times = 0;
 	cin >> s;
 	for(int n = 2; n <= s; n++) {
 		if(isPrime(n) == 1) {
@@ -14,15 +14,18 @@ int main() {
 		}
 	}
 	cout << endl << times;
-};
+}
 
-int isPrime(int n){
+int isPrime(int n)
+{
 	int flag = 1;
-	for(int i=2; i<n; i++) {
-		if(n % i == 0) {
+	for(int i = 2; i < n; i++)
+	{
+		if(n % i == 0) 
+		{
 			flag = 0;
 			break;
 		}
-		return(flag);
 	}
+	return(flag);//for每循环一次，将执行下面的语句，而不是整部循环。 
 }
