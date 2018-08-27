606e61d6fe8b1fb2b45aa73dfc05b52ddc2eb8ca
Null <pm2966434705@163.com>
2018-08-27 17:13:04 +0800

    Update 8-27-2018_bubble_sorting.cpp

--- 8-27-2018_bubble_sorting.cpp
+++ 8-27-2018_bubble_sorting.cpp
@@ -13,10 +13,9 @@ int main() {
 	}
 	//Bubble Sorting.
 	for(i = 0; i < n - 1; i++) {
-		for(j = 0; j < n - i - 1; j++){
+		for(j = 0; j < n - i - 1; j++) {
 			if(a[j] > a[j + 1]) {
-				int 
-				t = a[j];
+				int	t = a[j];
 				a[j] = a[j + 1];
 				a[j + 1] = t;
 			}
