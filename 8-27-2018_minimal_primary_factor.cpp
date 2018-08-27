d9d4eca1f26b1a49da89bf3c04ba3dca3c29a274
Null <pm2966434705@163.com>
2018-08-27 16:06:20 +0800

    Create 8-27-2018_minimal_primary_factor.cpp

---
+++ 8-27-2018_minimal_primary_factor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, temp;
+	cout << "Input 2 numbers : ";
+	cin >> a;
+	cin >> b;
+	
+	if(a > b) {
+		while(b > 0) {
+			temp = a % b;
+			a = b;
+			b = temp;
+		}
+		cout << a;
+	} else {
+		while(a > 0) {
+			temp = b % a;
+			b = a;
+			a = temp;
+		}
+		cout << b;
+	}
+}
